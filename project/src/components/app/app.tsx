d1d6f9243bce13746072423d99c991113e7ede40
Яньшин Сергей Сергеевич <yanshin.s@medindex.ru>
2022-02-15 00:52:11 +0300

    feat(3.5): add a private route

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,12 +1,13 @@
 import Main from '../main/main';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NotFound from '../not-found/not-found';
-import { AppRoutes } from '../../constants';
+import { AppRoutes, AuthorizationStatus } from '../../constants';
 import Login from '../login/login';
 import MyList from '../my-list/my-list';
 import FilmPage from '../film-page/film-page';
 import AddReview from '../add-review/add-review';
 import Player from '../player/player';
+import PrivateRoute from '../private-route/private-route';
 
 type AppProps = {
   film: {
@@ -21,7 +22,12 @@ function App({film}: AppProps): JSX.Element {
     <Router>
       <Routes>
         <Route path={AppRoutes.SignIn} element={<Login />} />
-        <Route path={AppRoutes.MyList} element={<MyList />} />
+        <Route path={AppRoutes.MyList} element={
+          <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+            <MyList />
+          </PrivateRoute>
+        }
+        />
         <Route path={AppRoutes.AddReview} element={<AddReview />} />
         <Route path={AppRoutes.Film} element={<FilmPage />} />
         <Route path={AppRoutes.Player} element={<Player />} />
--- project/src/constants.ts
+++ project/src/constants.ts
@@ -6,3 +6,9 @@ export enum AppRoutes {
   AddReview = '/films/:id/review',
   Player = '/player/:id',
 }
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
+}
